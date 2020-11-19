2df056f40230dd01f1ffcd0ce918bc5746550a5f
zxilly <zhouxinyu1001@gmail.com>
2020-11-19 19:49:02 +0800

    feat(project): add utils

---
+++ frontend/src/components/cheader.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: {
+    "user":String,
+  },
+  data:()=>({
+
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   vuetify,
