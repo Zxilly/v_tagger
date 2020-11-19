0cd96f7b5531aba489240ee1a3bb8b2c82fe50b8
zxilly <zhouxinyu1001@gmail.com>
2020-11-19 19:53:18 +0800

    feat(project): add vue-router
    Based on the need of the project,vue-router has been added to the
    project.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+
 import axios from 'axios'
+
+Vue.use(VueRouter)
+
 
 Vue.config.productionTip = false
 
