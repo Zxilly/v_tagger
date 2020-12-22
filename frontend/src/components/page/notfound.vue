1d19558bce86952e33bc9c234d3d98cc901fcdfe
Zxilly <zhouxinyu1001@gmail.com>
2020-12-22 23:40:28 +0800

    feat(frontend): add router definition
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

---
+++ frontend/src/components/page/notfound.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "notfound"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router'
 
+import reg from '@/components/user/reg'
+import login from '@/components/user/login'
+
+import addtag from "@/components/work/addtag";
+import notfound from "@/components/page/notfound";
+
 import axios from 'axios'
 
 Vue.use(VueRouter)
@@ -12,7 +18,34 @@ Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios;
 
+
+const router = new VueRouter({
+  routes: [
+    {
+      path:'/',
+      component: App
+    },
+    {
+      path:'/user/reg',
+      component: reg
+    },
+    {
+      path:'/user/login',
+      component: login
+    },
+    {
+      path:'/work/addtag/:hash',
+      component: addtag
+    },
+    {
+      path:'*',
+      component: notfound
+    }
+  ]
+})
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
