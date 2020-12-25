61c9dd79f88789e65f9c42a9feff5bbb00866c08
Zxilly <zhouxinyu1001@gmail.com>
2020-12-25 13:40:07 +0800

    feat(frontend): add video tag
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- frontend/src/components/work/addtag.vue
+++ frontend/src/components/work/addtag.vue
@@ -1,23 +1,43 @@
 <template>
   <v-row>
     <v-col cols="8">
-      
+      <video-player class="video-player-box"
+                    ref="videoPlayer"
+                    :options="playerOptions"
+      >
+      </video-player>
     </v-col>
-    <v-col cols="8">
+    <v-col cols="4">
 
     </v-col>
   </v-row>
 </template>
 
 <script>
+import {apiurl} from "@/config";
+
 export default {
   name: "addtag",
   data: () => ({
-    saved: false
+    saved: false,
   }),
   computed: {
     hash: function () {
       return this.$route.params.hash
+    },
+    player() {
+      return this.$refs.videoPlayer.player
+    },
+    playerOptions:function (){
+      return {
+        // videojs options
+        muted: false,
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: apiurl+'/video/data/'+this.$route.params.hash+'.mp4'
+        }],
+      }
     }
   },
   beforeRouteLeave(to, from, next) {
