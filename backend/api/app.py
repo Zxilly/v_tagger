dd369a8f56fde4ca3f5d603680248cd02b56e8bf
Zxilly <zhouxinyu1001@gmail.com>
2021-03-05 16:24:17 +0800

    fix(backend): try to fix connection lost

--- backend/api/app.py
+++ backend/api/app.py
@@ -23,12 +23,15 @@ app.add_middleware(
 async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
 
-    if db.db.is_closed():
+    if not db.db.is_connection_usable():
         db.db.connect()
 
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
+
+
+
     return response
 
 
